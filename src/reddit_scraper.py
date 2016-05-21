5f46e33638ec1aea23d7bca6e97c022d3f74d483
Roman Kazarin <rkazarin@umail.ucsb.edu>
2016-05-21 14:15:38 -0700

    reddit scraper

---
+++ src/reddit_scraper.py
@@ -0,0 +1,41 @@
+import praw
+import sys
+import datetime
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+data = {}
+
+r = praw.Reddit(user_agent = "some random bullshit version 1.6577333")
+
+num_submissions = 0
+for s in praw.helpers.submissions_between(r, 'bitcoin'):
+	try:
+		num_submissions += 1
+		date_string = datetime.datetime.fromtimestamp(s.created).strftime('%m-%d-%Y')
+		if(date_string not in data):
+			data[date_string] = {}
+
+		key = (str(s.title), s.score)
+		data[date_string][key] = {}
+		# print("-----------------------------")
+		# print("Title: " + str(s.title))
+		# print("Link: " + str(s.short_link))
+		# print("Created at: " + str(s.created))
+		# print("Comments: ")
+		flat_comments = praw.helpers.flatten_tree(s.comments)
+		for comment in flat_comments:
+			data[date_string][key][str(comment.body)] = comment.score
+
+		print("Num submissions processed: " + str(num_submissions))
+
+		if(num_submissions % 100 == 0):
+			f = open("reddit_skeddit_backup.dat", "w")
+    		f.write(data)
+    		f.close()
+
+	except Exception as e:
+		print(e)
+		continue
+
